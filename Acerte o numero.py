427deb4df9986f9f2987bc3574ac1275729bcd1c
Lucas Gabriel Theodoro <89911712+LucasGabrielTheodoro@users.noreply.github.com>
2021-09-10 09:14:48 -0300

    Verão final 3
    msm motivo da "versão final"

--- Acerte o numero.py
+++ Acerte o numero.py
@@ -2,10 +2,11 @@ import random
 
 resu = random.randint (1, 10)
 resposta = 0
-x = 1 #X é as tentaivas, preguiça de mudar
+tentativas = 0 
 record1 = 1
 record2 = 1
 record3 = 2
+
 
 print ('''------NÍVEIS------
 1 = 15 tentativas
@@ -33,41 +34,41 @@ O jogo funciona assim:
 Você irá tentar acertar o número que o computador estiver pensando, ele vai mudar toda vez o núemero.
 -----------------------------------------------------------------------------------------------------''')
 
+
 if escolha == 3: 
     while resposta != resu:
+        tentativas = tentativas + 1
         resu = random.randint (1, 10)
         resposta = int(input('Digite um número entre 1 a 10: '))
-        print ('Já foram {}x de 5'.format (x))
-        x = x + 1 
-        if x == 5: 
+        print ('Já foram {}x de 5'.format (tentativas))
+        if tentativas == 5: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
 elif escolha == 2: 
     while resposta != resu: 
+        tentativas = tentativas + 1
         resu = random.randint (1, 10)
         resposta = int(input('Digite um número entre 1 a 10: '))
-        print ('Já foram {}x de 10'.format (x))
-        x = x + 1 
-        if x == 10: 
+        print ('Já foram {}x de 10'.format (tentativas))
+        if tentativas == 10: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
 elif escolha == 1: 
     while resposta != resu:  
+        tentativas = tentativas + 1
         resu = random.randint (1, 10)
         resposta = int(input('Digite um número entre 1 a 10: '))
-        print ('Já foram {}x de 15'.format (x))
-        x = x + 1 
-        if x == 15: 
+        print ('Já foram {}x de 15'.format (tentativas))
+        if tentativas == 15: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
 
-
 print ('--'*4)
 print ('Acabou')
-print ('Você tentou: {}x'.format (x))
+print ('Você tentou: {}x'.format (tentativas))
 
 if resu == resposta:
     print ('--'*4)
@@ -75,19 +76,19 @@ if resu == resposta:
 
 
 elif escolha == 3:
-	if x < 5:
+	if tentativas < 5:
 		record3 -= record3
-		record3 = x
-		print ("Você bateu o record do dificil, agora é: {}".format (x))
+		record3 = tentativas
+		print ("Você bateu o record do dificil, agora é: {}".format (tentativas))
 
 elif escolha == 2:
-	if x < 5:
+	if tentativas < 5:
 		record2 -= record2
-		record2 = x
-		print ("Você bateu o record do mediano, agora é: {}".format (x))
+		record2 = tentativas
+		print ("Você bateu o record do mediano, agora é: {}".format (tentativas))
 		
 if escolha == 1:
-	if x < 15:
+	if tentativas < 15:
 		record1 -= record1
-		record1 = x
-		print ("Você bateu o record do facil, agora é: {}".format (x))
+		record1 = tentativas
+		print ("Você bateu o record do facil, agora é: {}".format (tentativas))
