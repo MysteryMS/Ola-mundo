982dbf1b8ed684f44de46671a29db01f1a4bdf60
Lucas Gabriel Theodoro <89911712+LucasGabrielTheodoro@users.noreply.github.com>
2021-09-10 08:42:58 -0300

    Versão final
    Atualizei durante a palestra na escola (to na escola), por isso eu mandei direto pelo site

--- Acerte o numero.py
+++ Acerte o numero.py
@@ -1,12 +1,11 @@
-#Esse é o Beta, ainda ta em processo de desenvolvimento
 import random
 
 resu = random.randint (1, 10)
 resposta = 0
 x = 0 #X é as tentaivas, preguiça de mudar
-record1 = 15
-record2 = 5
-record3 = 5
+record1 = 6
+record2 = 3
+record3 = 4
 
 print ('''------NÍVEIS------
 1 = 15 tentativas
@@ -14,62 +13,76 @@ print ('''------NÍVEIS------
 3 = 5 tentativas ''')
 print ("--"*9)
 print ("""Os records são:
-Nivel 1= 5x
-Nivel 2= 5x
-Nivel 3= 15x""")
+Nivel 1= {}x
+Nivel 2= {}x
+Nivel 3= {}x""".format (record1, record2, record3))
 print ("--"*9)
+duvidas = input('Você quer entender como funciona? [S/N]: ').upper ()
 escolha = int(input('Digite um número para escolher a difículdade [1/2/3]: '))
 
+if duvidas == "S":
+    print ('''-----------------------------------------------------------------------------------------------------
+O jogo funciona assim:
+
+Você irá tentar acertar o número que o computador estiver pensando, ele vai mudar toda vez o núemero.
+-----------------------------------------------------------------------------------------------------''')
+
 if escolha == 3: 
-    while resposta != resu: 
-        
+    while resposta != resu:
+        x = x + 1  
         resu = random.randint (1, 10)
-        print (resu)
         resposta = int(input('Digite um número entre 1 a 10: '))
-        x = x + 1 
+        print ('Já foram {}x de 5'.format (x))
         if x == 5: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
 if escolha == 2: 
     while resposta != resu: 
-        
-        resu = random.randint (1, 10)
-        print (resu)
-        resposta = int(input('Digite um número entre 1 a 10: '))
         x = x + 1 
+        resu = random.randint (1, 10)
+        resposta = int(input('Digite um número entre 1 a 10: '))
+        print ('Já foram {}x de 10'.format (x))
         if x == 10: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
 if escolha == 1: 
     while resposta != resu: 
+        x = x + 1 
         resu = random.randint (1, 10)
-        print (resu)
         resposta = int(input('Digite um número entre 1 a 10: '))
+        print ('Já foram {}x de 15'.format (x))
         x = x + 1 
         if x == 15: 
             print ('Você atingiu o maximo de tentativas possiveis')
             break
 
-print ('-=-'*4)
+
+
+print ('--'*4)
 print ('Acabou')
-print ("Tentativas: {}".format (x))
+print ('Você tentou: {}x'.format (x))
+
+if resu == resposta:
+    print ('--'*4)
+    print ('Você acertou o número')
+
 
 if escolha == 3:
 	if x < 5:
 		record3 -= record3
 		record3 = x
-		print ("Você bateu o record do dificil, agora o record é: {}".format (x))
+		print ("Você bateu o record do dificil, agora é: {}".format (x))
 
 if escolha == 2:
 	if x < 5:
 		record2 -= record2
 		record2 = x
-		print ("Você bateu o record do mediano, agora o record é: {}".format (x))
+		print ("Você bateu o record do mediano, agora é: {}".format (x))
 		
 if escolha == 1:
 	if x < 15:
 		record1 -= record1
 		record1 = x
-		print ("Você bateu o record do facil, agora o record é: {}".format (x))
+		print ("Você bateu o record do facil, agora é: {}".format (x))
